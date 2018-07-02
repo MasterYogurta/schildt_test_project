34bf1a12097cae7ec42db1f99b33bfebbbfe047f
Unknown <pavlusius@gmail.com>
2018-07-02 22:32:56 +0300

    Operations Example
    Updated ExampleOperations

--- Examples/ExampleOperations.java
+++ Examples/ExampleOperations.java
@@ -10,5 +10,34 @@ class ExampleOperations{
         System.out.println("[&= 0xFEED0000] operation :\t" + String.format("0x%08X", (vairable &= 0xFEED0000)));
         System.out.println("[|= 0xCAFE] operation :\t\t" + String.format("0x%08X", (vairable |= 0xCAFE)));
         System.out.println("[^= 0xEDCA00] operation :\t" + String.format("0x%08X", (vairable ^= 0xEDCA00)));
+        System.out.println("-------------------------------------------------------------------------------");
+
+        boolean testVar1 = true, testVar2;
+        System.out.println("testVar1 :\t" + testVar1);
+
+        testVar2 = ((2 < 0) | (1 > 0));
+        System.out.println("testVar2 | :\t" + testVar2);
+        testVar2 = ((2 < 0) & (1 > 0));
+        System.out.println("testVar2 & :\t" + testVar2);
+        testVar2 = ((2 < 0) || (1 > 0));
+        System.out.println("testVar2 || :\t" + testVar2);
+        testVar2 = ((2 < 0) && (1 > 0));
+        System.out.println("testVar2 && :\t" + testVar2);
+        System.out.println("-------------------------------------------------------------------------------");
+
+        int counter = 0;
+        testVar2 = ((2 < 0) | (counter++ > 0));
+        System.out.println("testVar2 | :\t" + testVar2);
+        System.out.println("counter :\t" + counter);
+        testVar2 = ((2 < 0) & (counter++ > 0));
+        System.out.println("testVar2 & :\t" + testVar2);
+        System.out.println("counter :\t" + counter);
+        testVar2 = ((2 < 0) || (counter++ > 0));
+        System.out.println("testVar2 || :\t" + testVar2);
+        System.out.println("counter :\t" + counter);
+        testVar2 = ((2 < 0) && (counter++ > 0));
+        System.out.println("testVar2 && :\t" + testVar2);
+        System.out.println("counter :\t" + counter);
+        System.out.println("-------------------------------------------------------------------------------");
     }
 }
