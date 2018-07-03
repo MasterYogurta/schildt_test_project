c32b5821174e6c69ba704a8e8f653d053074b52f
Unknown <pavlusius@gmail.com>
2018-07-03 20:53:12 +0300

    Example ForEach
    Updated ExampleForEach.java

--- Examples/ExampleForEach.java
+++ Examples/ExampleForEach.java
@@ -2,21 +2,31 @@
 
 class ExampleForEach{
     public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------------------------------");
         // Display input parameters using for each
         System.out.println("Display input parameters using for each");
         int counter = 1;
         BananaCheck: for (String str : args){
             System.out.println("input parameter #" + (counter++) + " : " + str);
+            // Check input parameter on banana presence
             if (isStringContainsSubstringIgnoreCase(str, "banana")){
                 System.out.println("Banana alert! Abandon cicle!");
                 break BananaCheck;
             }
         }
         System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Display input parameters as Character matrix");
         char[][] matrix;
         matrix = convertStringArrayToCharMatrix(args);
-        System.out.println("Display input parameters as Character matrix");
         displayInputCharMatrix(matrix);
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Display input parameter only if it contains [apple]");
+        counter = 0;
+        AppleSearch: for (String str : args){
+            counter++;
+            if (!isStringContainsSubstringIgnoreCase(str, "apple")) continue AppleSearch;
+            System.out.println("Apple was found in parameter #" + counter +" [" + str + "]");
+        }
     }
 
     /**
@@ -50,7 +60,7 @@ class ExampleForEach{
     /**
       * @brief  Check if given string contains specify substring
       * @param  sourceString: Main source string
-      * @param  subString: Substring for serch
+      * @param  subString: Substring for search
       * @return result: Boolean search endcode: true or false
       */
     private static boolean isStringContainsSubstringIgnoreCase(String sourceString, String subString){
